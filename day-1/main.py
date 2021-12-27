bdfb2b13a71c01544797d016709d0c8b8aec7cad
Risky Kurniawan <96241592+riskykrnawan@users.noreply.github.com>
2021-12-27 18:10:21 +0800

    day 1 done

---
+++ day-1/main.py
@@ -0,0 +1,5 @@
+# Band Name
+print("Selamat datang di Band Name Generator.")
+street = input("Apa nama kota tempat anda tinggal?\n")
+pet = input("Apa nama Hewan Peliharaanmu?\n")
+print("Nama Band mu adalah: " + street + " " + pet)
